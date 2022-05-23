da0f0dfc4d4a99c9ebc1e7378f5d003d5044e65c
emascapa <emascapa@gmail.com>
2022-05-23 11:58:09 +0200

    add bootstrap/snack-3

--- snack-3/index.php
+++ snack-3/index.php
@@ -59,19 +59,28 @@ $posts = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="text-center p-4 bg-dark">
 
     <?php foreach ($posts as $key => $value) { ?>
 
-        <h2><?php echo $key; ?></h2>
-        <ul>
+        <h2 class="text-light"><?php echo $key; ?></h2>
+        <ul class="list-unstyled">
             <?php foreach ($value as $post) { ?>
                 <li>
-                    <h4><?php echo $post['title']; ?></h4>
-                    <h6><?php echo $post['author']; ?></h6>
-                    <p><?php echo $post['text']; ?></p>
+                    <div class="rounded rounded-4 border border-2 border-dark my-3 p-2 bg-light d-flex flex-column mx-auto">
+
+                        <img class="rounded rounded-4 mb-2" src="http://picsum.photos/500/200" alt="">
+                        
+                        <h4><?php echo $post['title']; ?></h4>
+                        <h6><?php echo $post['author']; ?></h6>
+                        <p class="mb-0"><?php echo $post['text']; ?></p>
+                        
+                    </div>
+   
                 </li>
             <?php } ?>
         </ul>
